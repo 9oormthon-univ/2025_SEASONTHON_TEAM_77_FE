077e8da5622048425a038722fc5c16bdf28a2115
Jiyoon <SAMSUNG@Jiyoons-MacBook-Air.local>
2025-09-05 02:15:06 +0900

    feat: LearnMenu 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import LearnStructure from './features/kiosk/learn-structure/LearnStructure'
 import OrderStart from './features/kiosk/learn-order/OrderStart'
+import LearnMenu from './features/kiosk/learn-menu/LearnMenu'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/teachmap/kioskstructure" element={<LearnStructure />} />
       <Route path="/teachmap/kioskorder" element={<OrderStart />} />
+      <Route path="/teachmap/kioskmenu" element={<LearnMenu />} />
     </Routes>
   )
 }
