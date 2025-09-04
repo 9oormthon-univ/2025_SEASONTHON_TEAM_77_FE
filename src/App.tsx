1bfca68ca3a59077afae4b600a8cdd938f1b1585
hyeran1216 <hran1216@khu.ac.kr>
2025-09-04 19:54:39 +0900

    feat: login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 import LearnStructure from './features/kiosk/learn-structure/LearnStructure'
 import OrderStart from './features/kiosk/learn-order/OrderStart'
+import LoginForm from './features/login/LoginForm'
+import SignupForm from './features/login/SignupForm'
 
 function App() {
 
@@ -8,6 +10,8 @@ function App() {
     <Routes>
       <Route path="/teachmap/kioskstructure" element={<LearnStructure />} />
       <Route path="/teachmap/kioskorder" element={<OrderStart />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/signup" element={<SignupForm />} />
     </Routes>
   )
 }
