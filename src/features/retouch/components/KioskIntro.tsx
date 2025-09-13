4ec9a7f3a60078d2107050456e6adc853994489d
Jiyoon <SAMSUNG@Jiyoons-MacBook-Air.local>
2025-09-13 18:46:03 +0900

    chore: 리터치 매장선택 화면 ui 수정

--- src/features/retouch/components/KioskIntro.tsx
+++ src/features/retouch/components/KioskIntro.tsx
@@ -29,8 +29,8 @@ export default function KioskIntro({
       <KioskFrame disableTabClicks forcedTotals={totals}>
         {introPhase === 'select' ? (
           <div 
-            className="flex flex-col items-center justify-center w-[317px] h-[567px] py-[131px] -pr-4 px-[16px] border-2 border-gray-300 bg-white"
-            style={{ boxShadow: '0 2px 4px 0 rgba(0,0,0,0.04)', borderRadius: '36px' }}
+            className="flex flex-col items-center justify-center w-[315px] h-[565px] py-[131px] border-2 border-gray-300 bg-white"
+            style={{ boxShadow: '0 2px 4px 0 rgba(0,0,0,0.04)', borderRadius: '34px' }}
           >
             <h3 className="text-xl text-black text-center mb-9" style={{ fontFamily: 'Pretendard', fontWeight: 600, lineHeight: '140%' }}>
               주문 방법을 선택해 주세요
@@ -44,9 +44,13 @@ export default function KioskIntro({
                 }`}
                 style={{ border: '1px solid #ECECEC', boxShadow: '0 2px 4px 0 rgba(0,0,0,0.04)' }}
               >
-                <svg xmlns="http://www.w3.org/2000/svg" width="59" height="58" viewBox="0 0 59 58" fill="none">
-                  <path d="M52.75 32.2088V49.667H55.3333V54.8337H3.66665V49.667H6.24998V32.2088C4.65961 31.1473 3.35591 29.7096 2.4546 28.0232C1.55329 26.3369 1.08226 24.4541 1.08331 22.542C1.08331 20.4056 1.66198 18.3467 2.71856 16.5926L9.72456 4.45866C9.95129 4.06595 10.2774 3.73984 10.6701 3.51311C11.0628 3.28638 11.5083 3.167 11.9617 3.16699H47.0408C47.4943 3.167 47.9397 3.28638 48.3324 3.51311C48.7251 3.73984 49.0513 4.06595 49.278 4.45866L56.2582 16.5538C57.7993 19.1136 58.2946 22.1688 57.6413 25.0844C56.9879 27.9999 55.2361 30.5515 52.75 32.2088Z" fill="#444444"/>
-                </svg>
+                <img
+                  src="/src/assets/store.png"
+                  alt="store"
+                  width={59}
+                  height={58}
+                  className="inline-block"
+                />
                 <h3 className="text-xl text-[#444444] text-center mt-2" style={{ fontFamily: 'Pretendard', fontWeight: 600, lineHeight: '140%' }}>매장</h3>
                 <h3 className="text-sm text-[#444444] text-center mt-2" style={{ fontFamily: 'Pretendard', fontWeight: 500, lineHeight: '140%' }}>To Order</h3>
               </button>
@@ -58,9 +62,13 @@ export default function KioskIntro({
                 }`}
                 style={{ border: '1px solid #ECECEC', boxShadow: '0 2px 4px 0 rgba(0,0,0,0.04)' }}
               >
-                <svg xmlns="http://www.w3.org/2000/svg" width="59" height="58" viewBox="0 0 59 58" fill="none">
-                  <path d="M17.4166 12.0837V4.83366C17.4166 4.19272 17.6713 3.57803 18.1245 3.12482C18.5777 2.6716 19.1924 2.41699 19.8333 2.41699H39.1666V12.0837H17.4166Z" fill="#444444"/>
-                </svg>
+                <img
+                  src="/src/assets/to_go.png"
+                  alt="store"
+                  width={59}
+                  height={58}
+                  className="inline-block"
+                />
                 <h3 className="text-xl text-[#444444] text-center mt-2" style={{ fontFamily: 'Pretendard', fontWeight: 600, lineHeight: '140%' }}>포장</h3>
                 <h3 className="text-sm text-[#444444] text-center mt-2" style={{ fontFamily: 'Pretendard', fontWeight: 500, lineHeight: '140%' }}>To Go</h3>
               </button>
